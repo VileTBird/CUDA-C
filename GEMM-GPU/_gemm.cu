85001e1c028cac5ac97a94d769763f6eb0519871
vileTbird <shridharapavel@gmail.com>
2025-04-24 16:54:09 -0400

    gemm repetition again again

--- GEMM-GPU/_gemm.cu
+++ GEMM-GPU/_gemm.cu
@@ -1,72 +1,44 @@
 # include <iostream>
-# define N 2
+# define count 2
 
 
-__global__ void gemmGPU(int *a, int *b, int *c, int m, int n, int K)
+__global__ void gemmGPU(int *a, int *b, int *c, int M, int N, int K)
 {
-    // flipping row/col id doesnt have an effect on correctness but it canimpact 
-    // impact performance due to something called memory coaelscing and data locality
-    // which i have no idea what it means but i will learn soon.
     int colId = threadIdx.x + blockIdx.x * blockDim.x;
     int rowId = threadIdx.y + blockIdx.y * blockDim.y;
 
-    /* although this logic works. i think it could be expanded even further for by breaking down
-    entire matrices into chunks or tiles, which means each thread would process much lesser 
-    numbner of elements and elements that are closer to each other in memory,this would mean that
-    they accumulate the dot product for the same index i, j, then finally when all tiles compute for i, j
-    they just add to the whole. achieving more concurrency, i think thats how tiling should work
-    but fyi i haveno idea how actual tiling works this is just my idea for further optimiztion
-    */
-    while(rowId < m && colId < n)
+    while(rowId < M && colId < N)
     {
-        c[rowId * n + colId] = 0;
         for(int k = 0; k < K; k++)
         {
-            c[rowId * n + colId] += a[rowId * K + k] * b[k * n + colId];
+            c[rowId * N + colId] += a[rowId * K + k] * b[k * N + colId];
         }
+        rowId += blockDim.y * gridDim.y;
 
         colId += blockDim.x * gridDim.x;
-        rowId += blockDim.y * gridDim.y;
     }
-
 }
-
-void printMatrix(int *a, int m, int n)
+void printMatrix(int *a, int M, int N)
 {
-    for(int i = 0; i < m; i++)
-    {
-        for(int j = 0; j < n; j++)
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++)
         {
-            printf(" %d ", a[i * n + j]);
+            printf(" %d ", a[i * N + j]);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void gemmCPU(int *a, int *b, int *c, int m, int n, int K)
+void gemmCPU(int *a, int *b, int *c, int M, int N, int K)
 {
-    // m x k -> dimension of first matrix a
-    // k x n -> dimension of second matrix b
-    // m x n -> dimension of resulting matrix c
-
-    // for each row in matrix a
-    for(int i = 0; i < m; i++)
+    for(int i = 0; i < M; i++)
     {
-        // for each column in matrix b
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < N; j++)
         {
-            // for each element in both row and column of matrix a, b
             for(int k = 0; k < K; k++)
             {
-                // for each element i, j in matrix c, for each element k 
-                // in row i * K of matrix a, we multiply with each element j
-                // in each row k * N of matrix b. 
-                // in other words one we iteratue thru each element in row of matrix 1
-                // by offsetting each elemnt by k for each row i * k 
-                // simultaneously we use row major order with k * n, to jump to
-                // each row and get the j corresponding element of matrix b which would give us columns
-                c[i * n + j] += a[i * K + k] * b[k * n + j];
+                c[i * N + j] += a[i * K + k] * b[k * N + j];
             }
         }
     }
@@ -76,61 +48,49 @@ int main(void)
 {
     int *a, *b, *c;
 
-    size_t size = N * N * sizeof(int);
+    size_t size = count * count * sizeof(int);
 
     a = (int*) malloc(size);
     b = (int*) malloc(size);
     c = (int*) malloc(size);
 
-    for(int i = 0; i < N * N; i++)
+    for (int i = 0;  i < count * count; i++)
     {
         a[i] = i;
         b[i] = i;
     }
 
-    printMatrix(a, N, N);
+    gemmCPU(a, b, c, count, count, count);
 
-    printMatrix(b, N, N);
+    printMatrix(a, count, count);
+    printMatrix(b, count, count);
+    printMatrix(c, count, count);
 
-    gemmCPU(a, b, c, N, N, N);
+    int *d = (int*) malloc(size);
 
-    printMatrix(c, N, N);
-
-    int *d;
     int *d_a, *d_b, *d_c;
-    d = (int*) malloc(size);
 
-    cudaMalloc((void**)&d_c, size);
     cudaMalloc((void**)&d_a, size);
-	cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_c, size);
 
     cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
-    
-    dim3 blockDim(12, 12, 1);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
-    int blockCount = 0;
-    if (N > blockDim.x)
-    {
-        blockCount = (N+11)/12;
-    }
-    else
-    {
-        blockCount = 32;
-    }
-    dim3 gridDim(blockCount, blockCount, 1);
-    gemmGPU<<<gridDim, blockDim>>>(d_a, d_b, d_c, N, N, N);
+    dim3 blockDim(16, 16);
+    dim3 gridDim(count+15/16, count+15/16);
+    gemmGPU<<<gridDim, blockDim>>>(a, b, d, count, count, count);
 
-    cudaMemcpy(d, d_c, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 
-    printMatrix(d, N, N);
-    cudaFree(d_c);
-    cudaFree(d_a);
-	cudaFree(d_b);
-	free(d);
-
+    printMatrix(d, count, count);
     free(a);
     free(b);
     free(c);
+    free(d);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
     return 0;
+
 }
